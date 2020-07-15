472e3ae9e26163a4d9ddf5656653a24cd3ef86dc
luzialeticia <leticialuzia@tutamail.com>
2020-07-15 03:01:22 -0300

    Coloquei alguns cometários no JS pra guiar o trabalho

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* green.value
+adiciona class em algum html classList.add()
+var = document.querySelector('#green');
+textContent: muda o conteúdo textual do elemento que for selecionado
+
+var div = document.querySelector('div);
+div.style.backgroundColor = "aqui vai alguma variável ou função que mude a cor do quadrado"
+*/
