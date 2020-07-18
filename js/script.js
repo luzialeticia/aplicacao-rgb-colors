d6621dac1c3a87b6a891a4f0a1cdcde469d44c8e
luzialeticia <leticialuzia@tutamail.com>
2020-07-18 01:26:31 -0300

    Fiz a função pra mudar os valores nos input text

--- js/script.js
+++ js/script.js
@@ -8,13 +8,22 @@ div.style.backgroundColor = "aqui vai alguma variável ou função que mude a co
 */
 
 var input = document.querySelectorAll(".range");
-    for (var i = 0; i < input.length; i++) {
-        input[i].addEventListener("input", function(){
-            var red = document.getElementById("red").value,
-                green = document.getElementById("green").value,
-                blue = document.getElementById("blue").value;
+
+for (var i = 0; i < input.length; i++) {
+    input[i].addEventListener("input", function(){
+        var red = document.getElementById("red").value,
+            green = document.getElementById("green").value,
+            blue = document.getElementById("blue").value;
                 
-            var cores = document.getElementById("colors");
-            cores.style.background = "rgb(" + red + ", " + green + ", " + blue + ")";
-        });
-    }
+        var cores = document.getElementById("colors");
+        cores.style.background = "rgb(" + red + ", " + green + ", " + blue + ")";
+    });
+}
+
+for (var i = 0; i < input.length; i++) {
+    input[i].addEventListener("input", function() {
+        document.querySelector('#redText').value = red.value;
+        document.querySelector('#greenText').value = green.value;
+        document.querySelector('#blueText').value = blue.value;
+    });
+}
