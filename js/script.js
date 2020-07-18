e1f1c24a7b5e003a268118065e19972d362d0d56
luzialeticia <leticialuzia@tutamail.com>
2020-07-18 01:08:08 -0300

    Manipulando os valores e refletindo na div colors

--- js/script.js
+++ js/script.js
@@ -7,13 +7,14 @@ var div = document.querySelector('div);
 div.style.backgroundColor = "aqui vai alguma variável ou função que mude a cor do quadrado"
 */
 
-/*var input = document.querySelectorAll("input");
-        for(var i = 0; i < input.length; i++){
+var input = document.querySelectorAll(".range");
+    for (var i = 0; i < input.length; i++) {
         input[i].addEventListener("input", function(){
             var red = document.getElementById("red").value,
                 green = document.getElementById("green").value,
                 blue = document.getElementById("blue").value;
-                var display = document.getElementById("display");
-            display.style.background = "rgb(" + red + ", " + green + ", " + blue + ")";
+                
+            var cores = document.getElementById("colors");
+            cores.style.background = "rgb(" + red + ", " + green + ", " + blue + ")";
         });
-    }*/
+    }
