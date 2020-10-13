92c8684936e864d763ef94c630bea177f254191c
luzialeticia <leticialuzia@tutamail.com>
2020-10-12 22:13:32 -0300

    Inserindo input pra receber o valor do rgb

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let input = document.querySelectorAll(".range");
 
 for (let i = 0; i < input.length; i++) {
-    input[i].addEventListener("input", function(){
+    input[i].addEventListener("input", () => {
         const red = document.getElementById("red").value,
               green = document.getElementById("green").value,
               blue = document.getElementById("blue").value;
@@ -12,9 +12,10 @@ for (let i = 0; i < input.length; i++) {
 }
 
 for (let i = 0; i < input.length; i++) {
-    input[i].addEventListener("input", function() {
+    input[i].addEventListener("input", () => {
         document.querySelector('#redText').value = red.value;
         document.querySelector('#greenText').value = green.value;
         document.querySelector('#blueText').value = blue.value;
+        document.querySelector("#rgbValue").value = `${red.value}, ${green.value}, ${blue.value}`
     });
 }
