fe5aec69e398ca9f44aa7c924921bb31d9ea58e7
luzialeticia <leticialuzia@tutamail.com>
2020-10-12 21:58:25 -0300

    Refatorando js pra usar ES6

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-var input = document.querySelectorAll(".range");
+let input = document.querySelectorAll(".range");
 
-for (var i = 0; i < input.length; i++) {
+for (let i = 0; i < input.length; i++) {
     input[i].addEventListener("input", function(){
-        var red = document.getElementById("red").value,
-            green = document.getElementById("green").value,
-            blue = document.getElementById("blue").value;
+        const red = document.getElementById("red").value,
+              green = document.getElementById("green").value,
+              blue = document.getElementById("blue").value;
                 
-        var cores = document.getElementById("colors");
+        const cores = document.getElementById("colors");
         cores.style.background = "rgb(" + red + ", " + green + ", " + blue + ")";
     });
 }
 
-for (var i = 0; i < input.length; i++) {
+for (let i = 0; i < input.length; i++) {
     input[i].addEventListener("input", function() {
         document.querySelector('#redText').value = red.value;
         document.querySelector('#greenText').value = green.value;
